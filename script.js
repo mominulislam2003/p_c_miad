bd2b300e8b8203b697d4efc104644e3ffbaab40f
Mominul Islam <mominulislam.miad@gmail.com>
2025-10-27 20:43:34 +0600

    Update script.js

--- script.js
+++ script.js
@@ -37,22 +37,23 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-<script>
-  const faders = document.querySelectorAll('.fade-in');
+// <script>
+//   const faders = document.querySelectorAll('.fade-in');
 
-  const appearOptions = {
-    threshold: 0.3,
-  };
+//   const appearOptions = {
+//     threshold: 0.3,
+//   };
 
-  const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-    entries.forEach(entry => {
-      if (!entry.isIntersecting) return;
-      entry.target.classList.add('show');
-      observer.unobserve(entry.target);
-    });
-  }, appearOptions);
+//   const appearOnScroll = new IntersectionObserver(function(entries, observer) {
+//     entries.forEach(entry => {
+//       if (!entry.isIntersecting) return;
+//       entry.target.classList.add('show');
+//       observer.unobserve(entry.target);
+//     });
+//   }, appearOptions);
 
-  faders.forEach(fader => {
-    appearOnScroll.observe(fader);
-  });
-</script>
+//   faders.forEach(fader => {
+//     appearOnScroll.observe(fader);
+//   });
+// </script>
+
