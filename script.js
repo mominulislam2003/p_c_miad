238b9afffd6fd99a8b033b0300a579fd6f38c6b3
Mominul Islam <mominulislam.miad@gmail.com>
2025-10-27 21:21:20 +0600

    Refactor script.js for improved readability

--- script.js
+++ script.js
@@ -37,23 +37,3 @@ document.addEventListener("DOMContentLoaded", function () {
   }
 });
 
-// <script>
-//   const faders = document.querySelectorAll('.fade-in');
-
-//   const appearOptions = {
-//     threshold: 0.3,
-//   };
-
-//   const appearOnScroll = new IntersectionObserver(function(entries, observer) {
-//     entries.forEach(entry => {
-//       if (!entry.isIntersecting) return;
-//       entry.target.classList.add('show');
-//       observer.unobserve(entry.target);
-//     });
-//   }, appearOptions);
-
-//   faders.forEach(fader => {
-//     appearOnScroll.observe(fader);
-//   });
-// </script>
-
